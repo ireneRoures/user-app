c4962e30db8e62e6e5198c637ab4e4d2159b1285
ireneR <irenerouresvillalonga@gmail.com>
2021-01-13 12:06:02 +0100

    Traducciones

---
+++ src/translations/translationES.ts
@@ -0,0 +1,8 @@
+export const translationES = {
+    'TR_USER_LIST': 'Listado de usuario',
+    'TR_USER_DETAILS': 'Detalles de usuario',
+    'TR_NAME': 'Nombre',
+    'TR_EMAIL': 'Email',
+    'TR_ID': 'ID',
+    'TR_USERS': 'Usuarios'
+}
